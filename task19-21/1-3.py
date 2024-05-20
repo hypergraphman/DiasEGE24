4acb4fddbe48dcf177af2c020b76e906b0b918a8
r_aitov <r_aitov@mail.ru>
2024-05-20 22:43:49 +0300

    task 19-21

--- task19-21/1-3.py
+++ task19-21/1-3.py
@@ -1,16 +1,19 @@
-def f(s, c, m):
+def f(s, c, m, b):
     if c > m:
         return False
     if s >= 141:
         return c % 2 == m % 2
-    moves = [f(s + 1, c + 1, m),
-             f(s * 2, c + 1, m)]
+    moves = []
+    if b[0] == 0:
+        moves.append(f(s + 1, c + 1, m, (1, 0)))
+    if b[1] == 0:
+        moves.append(f(s * 2, c + 1, m, (0, 1)))
     return any(moves) if (c + 1) % 2 == m % 2 else all(moves)
 
 
 for s in range(1, 141):
     for m in range(1, 5):
-        if f(s, 0, m):
+        if f(s, 0, m, (0, 0)):
             if m == 4:
                 print(s)
             break
